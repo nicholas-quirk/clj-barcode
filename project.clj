649c026fe221ed2f7e91765597a08675c1868456
Deon Moolman <cmdrdats@gmail.com>
2012-10-17 21:58:04 +0200

    Use the 1.5-beta1 barbecue jar that is in the public repo

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.sourceforge/barbecue "1.0.6d"]]
+                 [net.sourceforge.barbecue/barbecue "1.5-beta1"]]
   :main clj-barcode.core)
